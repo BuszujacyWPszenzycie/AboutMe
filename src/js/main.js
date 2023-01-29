790167df724b85f6205fa084cf54865fcd5a3f88
Marcin <marcin.szturminski@gmail.com>
2023-01-29 18:51:05 +0100

    added aos and main sentence

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 const hamburgerBtn = document.querySelector('.nav__hamburger-btn')
 const navMobileList = document.querySelector('.nav__mobile-list')
 const navShadow = document.querySelector('.nav__mobile-shadow')
+const helloPage = document.querySelector('.hello__wrapper')
+
+console.log(helloPage)
 
 const handleBurgerBtn = () => {
 	hamburgerBtn.classList.toggle('is-active')
@@ -8,4 +11,9 @@ const handleBurgerBtn = () => {
 	navShadow.classList.toggle('nav__mobile-shadow-active')
 }
 
+const deacitvateHelloPage = () => {
+	helloPage.classList.add('hello__wrapper-deactivate')
+}
+
 hamburgerBtn.addEventListener('click', handleBurgerBtn)
+setTimeout(deacitvateHelloPage, 2000)
