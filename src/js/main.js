341f95c90b40384356dc27e87520f4c3bf32d154
Marcin <marcin.szturminski@gmail.com>
2023-01-29 17:02:53 +0100

    complete desktop nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 const hamburgerBtn = document.querySelector('.nav__hamburger-btn')
 const navMobileList = document.querySelector('.nav__mobile-list')
-const navShadow = document.querySelector('.nav__shadow')
+const navShadow = document.querySelector('.nav__mobile-shadow')
 
 const handleBurgerBtn = () => {
 	hamburgerBtn.classList.toggle('is-active')
 	navMobileList.classList.toggle('nav__mobile-list-active')
-	navShadow.classList.toggle('nav__shadow-active')
+	navShadow.classList.toggle('nav__mobile-shadow-active')
 }
 
 hamburgerBtn.addEventListener('click', handleBurgerBtn)
